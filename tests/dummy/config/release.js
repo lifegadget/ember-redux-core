a825bc0956a7e8335f9acc8e73bfda0adc5b15c9
Ken Snyder <ken@ken.net>
2017-01-29 23:49:59 -0800

    update release to auto publish to npm

---
+++ tests/dummy/config/release.js
@@ -0,0 +1,14 @@
+/* jshint node:true */
+
+// For details on each option run `ember help release`
+module.exports = {
+  // local: true,
+  // remote: 'some_remote',
+  // annotation: "Release %@",
+  // message: "Bumped version to %@",
+  // manifest: [ 'package.json', 'bower.json', 'someconfig.json' ],
+  publish: true,
+  // strategy: 'date',
+  // format: 'YYYY-MM-DD',
+  timezone: 'America/Los_Angeles'
+};
